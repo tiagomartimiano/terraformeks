4fa1cc9dd8a8efc8489a22511d55b1bffd868b9b
tiagomartimiano <t.uruba.vortigo@jazztech.com.br>
2025-05-22 23:35:02 -0300

    feat: private subnets and variables

--- public.tf
+++ public.tf
@@ -6,7 +6,7 @@ resource "aws_subnet" "eks_subnet_public_1a" {
   tags = merge(
     local.tags,
     {
-      Name                     = "comunidadedevops-subnet-1a",
+      Name                     = "${var.project_name}-pub-subnet-1a",
       "kubernetes.io/role/elb" = 1
     }
   )
@@ -20,7 +20,7 @@ resource "aws_subnet" "eks_subnet_public_1b" {
   tags = merge(
     local.tags,
     {
-      Name                     = "comunidadedevops-subnet-2a",
+      Name                     = "${var.project_name}-pub-subnet-1b",
       "kubernetes.io/role/elb" = 1
     }
   )
--- vpc.tf
+++ vpc.tf
@@ -5,7 +5,7 @@ resource "aws_vpc" "eks_vpc" {
   tags = merge(
     local.tags,
     {
-      name = "comunidadedevops=vpc"
+      name = "${var.project_name}=vpc"
     }
   )
 }
